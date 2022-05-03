7a56f5048cb8dda84c155ade2ed1c807240120e4
Alex <a.a.alexashkin@mail.ru>
2022-05-03 11:35:05 +0300

    base_commit

---
+++ application/main.py
@@ -0,0 +1,48 @@
+import json
+import requests
+
+from flask import Flask, request
+from sqlalchemy.exc import IntegrityError
+
+from database.database import DatabaseOperator
+
+app = Flask(__name__)
+DB = DatabaseOperator()
+
+@app.route('/', methods=["POST"])
+def index():
+	data = json.loads(request.get_data())
+	if "questions_num" in data.keys() and type(data["questions_num"]) == int:
+		for i in range(data["questions_num"]):
+			while True:
+				result = make_side_request()
+				data = prepare_data(result)
+				try:
+					DB.add_question(data)
+					break
+				except IntegrityError:
+					DB.session.rollback()
+					pass
+
+	return create_response_data(DB.get_last_record())
+
+
+def make_side_request():
+	return json.loads(requests.get("https://jservice.io/api/random?count=1").content)[0]
+
+
+def prepare_data(data):
+	return {"question_id": data["id"], "text": data["question"], "answer": data["answer"], "creation_date": data["created_at"]}
+
+
+def create_response_data(db_object):
+	return json.dumps({
+		"question_id": db_object.question_id,
+		"text": db_object.text,
+		"answer": db_object.answer,
+		"creation_date": db_object.creation_date.strftime("%d/%m/%Y, %H:%M:%S")
+	})
+
+
+app.run()
+
---
+++ database/database.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
+
+
+class DatabaseOperator:
+	settings = {
+		"drivername": "postgresql+psycopg2",
+		"host": "localhost",
+		"port": "5432",
+		"username": "postgres",
+		"password": "admin",
+		"database": "questions_db"
+	}
+	declarative_db = declarative_base()
+	db_engine = create_engine(
+		f"{settings['drivername']}://{settings['username']}:{settings['password']}@{settings['host']}:{settings['port']}/{settings['database']}",
+		pool_recycle=7200
+	)
+
+	class Questions(declarative_db):
+		__tablename__ = "questions"
+		id = Column(Integer, primary_key=True)
+		question_id = Column(Integer, unique=True)
+		text = Column(Text)
+		answer = Column(String)
+		creation_date = Column(DateTime)
+
+		def __init__(self, question_id: int, text: str, answer: str, creation_date: str):
+			self.question_id, self.text, self.answer, self.creation_date = question_id, text, answer, creation_date
+
+	def __init__(self):
+		if not database_exists(self.db_engine.url):
+			create_database(self.db_engine.url)
+		self.session = sessionmaker(bind=self.db_engine)()
+		self.declarative_db.metadata.create_all(self.db_engine)
+
+	def get_all_questions(self) -> list:
+		return self.session.query(self.Questions).all()
+
+	def add_question(self, data: dict):
+		new_question = self.Questions(**data)
+		self.session.add(new_question)
+		self.session.commit()
+
+	def get_last_record(self):
+		return self.session.query(self.Questions).order_by(self.Questions.id.desc()).first()
